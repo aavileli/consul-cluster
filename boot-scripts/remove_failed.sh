37d6aabc174c0ec42210ee972014481077385854
Paul Allen <pauldallen@gmail.com>
2015-06-16 15:59:36 -0700

    dont use the python helper

--- boot-scripts/remove_failed.sh
+++ boot-scripts/remove_failed.sh
@@ -8,13 +8,6 @@ cat <<"EOF" > "$script"
 region="$(curl -sL 169.254.169.254/latest/meta-data/placement/availability-zone | sed '$s/.$//')"
 my_ipaddress="$(curl -sL 169.254.169.254/latest/meta-data/local-ipv4)"
 
-addresses="$(python ./lib/group_addresses.py)"
-## lets wait until the minimum actually exists
-while [ "$(wc -l < <(echo $addresses | perl -pe 's{\s}{\n}g'))" -lt "${CONSUL_GROUP_SIZE_MIN:-0}" ]; do
-    sleep 1
-    addresses="$(python ./lib/group_addresses.py)"
-done
-
 failed_consul_nodes="$(/bin/consul members | grep failed | awk '{print $1}')"
 
 EOF
