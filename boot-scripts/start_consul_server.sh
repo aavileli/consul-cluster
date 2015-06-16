a839f578aba12663d49a91969e9a23883ac4cc26
Paul Allen <pauldallen@gmail.com>
2015-06-16 01:06:57 -0700

    /bin/consul not consul

--- boot-scripts/start_consul_server.sh
+++ boot-scripts/start_consul_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo "* * * * * ps -fwwe | grep -q \"[c]onsul agent\" || { nohup consul agent -config-dir /etc/consul/ & }" | crontab
+echo "* * * * * ps -fwwe | grep -q \"[c]onsul agent\" || { nohup /bin/consul agent -config-dir /etc/consul/ & }" | crontab
