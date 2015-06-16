c2885790bbb5e4d55577ae91e371eec87a87f50c
Paul Allen <pauldallen@gmail.com>
2015-06-16 01:20:51 -0700

    externalize start script

--- boot-scripts/start_consul_server.sh
+++ boot-scripts/start_consul_server.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+cat <<EOF > /opt/start_consul.sh
 #!/bin/sh
 
-echo "* * * * * ps -fwwe | grep -q \"[c]onsul agent\" || { nohup /bin/consul agent -config-dir /etc/consul/ & }" | crontab
+if ! ps -fwwe | grep -q "[c]onsul agent"; then
+    source /etc/profile
+    nohup /bin/consul agent -config-dir /etc/consul/ &
+fi
+
+EOF
+chmod +x /opt/start_consul.sh
+
+echo "* * * * * /opt/start_consul.sh" | crontab
