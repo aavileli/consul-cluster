fc093d1c60e3c28ad0107ee2522e2205c1b6d6cf
Paul Allen <pauldallen@gmail.com>
2015-06-16 00:57:55 -0700

    fixing grep

--- boot-scripts/start_consul_server.sh
+++ boot-scripts/start_consul_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo "* * * * * ps -fwwe | grep -q 'consul agent' || { nohup consul agent -config-dir /etc/consul/ & }" | crontab
+echo "* * * * * ps -fwwe | grep -q \"[c]onsul agent\" || { nohup consul agent -config-dir /etc/consul/ & }" | crontab
