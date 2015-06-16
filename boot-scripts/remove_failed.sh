0d5c83ab6869057a48199eb2e62f385828ca5025
Paul Allen <pauldallen@gmail.com>
2015-06-16 15:05:20 -0700

    cron out the removal process to all the boxes

--- boot-scripts/remove_failed.sh
+++ boot-scripts/remove_failed.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+scrpt="/opt/remove_failed.sh"
+
+cat <<"EOF" > $script
+#!/bin/bash
+
 region="$(curl -sL 169.254.169.254/latest/meta-data/placement/availability-zone | sed '$s/.$//')"
 my_ipaddress="$(curl -sL 169.254.169.254/latest/meta-data/local-ipv4)"
 
@@ -12,9 +17,15 @@ done
 
 failed_consul_nodes="$(/bin/consul members | grep failed | awk '{print $1}')"
 
+EOF
+
+## evaluate these variables
+cat <<EOF >> $script
 ## lets give it some time
 sleep "${CONSUL_FAILURE_GRACE_PERIOD_IN_SECONDS:-60}"
+EOF
 
+cat <<"EOF" >> $script
 for node in $failed_consul_nodes; do
     ## if they are still failed, kill them
     failed_node_name="$(/bin/consul members | grep $node | grep failed | awk '{print $1}')"
@@ -22,3 +33,13 @@ for node in $failed_consul_nodes; do
         /bin/consul force-leave "$failed_node_address"
     fi
 done
+EOF
+
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "* * * * * /bin/bash $script" >> mycron
+
+#install new cron file
+crontab mycron
+rm mycron
